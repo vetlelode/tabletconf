5d3d07668004790897b746c567206ba3ec91bc32
Vetle Lode <vetlelode@gmail.com>
2017-04-05 12:02:14 +0200

    More verbose logging + Other stuff

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-tablet_config
+#Simple Powershell script for configuring tablets with settings needed for deployment
+Thanks to the fine fellas over at https://github.com/steinwurf/adb-join-wifi for creating the wifi configuration app
--- installapps.ps1
+++ installapps.ps1
@@ -1,14 +1,16 @@
 ﻿adb devices
 adb shell settings put global install_non_market_apps 1
+Write-Output "Allowing Non Market Apps"
 adb shell settings put system screen_sleep 0
+Write-Output "Disabling System Sleep"
 adb shell settings put global wifi_on 1
+Write-Output "Disabling Sound Effects"
+adb shell settings put system sound_effects 0
 adb shell settings put global device_name PB-Finnsnes
-
-adb install apk\com.teamviewer.host.market.apk
-adb install apk\app-debug.apk
-adb install '.\apk\Kiosk Browser Lockdown_v2.5.6_apkpure.com.apk'
-adb reboot
-
-Start-Sleep -s 30
-
+Write-Output "Installing apps: \n"
+adb install -r apk\com.teamviewer.host.market.apk
+adb install -r apk\app-debug.apk
+adb install -r '.\apk\Kiosk Browser Lockdown_v2.5.6_apkpure.com.apk'
+Write-Output "Done."
 adb shell am start -n com.steinwurf.adbjoinwifi/com.steinwurf.adbjoinwifi.MainActivity -e ssid FF-Gjest
+Write-Output "Wifi configured, You should reboot to get all the settings in"
