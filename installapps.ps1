b4ab00c60c0676966f065d2ea3fb2413802867f9
Vetle Lode <vetlelode@gmail.com>
2017-04-05 12:56:47 +0200

    Added almost automatic config pushing

--- installapps.ps1
+++ installapps.ps1
@@ -7,10 +7,12 @@ adb shell settings put global wifi_on 1
 Write-Output "Disabling Sound Effects"
 adb shell settings put system sound_effects 0
 adb shell settings put global device_name PB-Finnsnes
-Write-Output "Installing apps: \n"
+Write-Output "Installing apps:"
 adb install -r apk\com.teamviewer.host.market.apk
 adb install -r apk\app-debug.apk
 adb install -r '.\apk\Kiosk Browser Lockdown_v2.5.6_apkpure.com.apk'
 Write-Output "Done."
 adb shell am start -n com.steinwurf.adbjoinwifi/com.steinwurf.adbjoinwifi.MainActivity -e ssid FF-Gjest
 Write-Output "Wifi configured, You should reboot to get all the settings in"
+adb push .\kioskbrowserconfig.xml /storage/sdcard0/kioskbrowserconfig.xml
+Write-Output "Pushing kiosk config"
