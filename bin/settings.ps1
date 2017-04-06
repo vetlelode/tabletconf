9995b4569f35898e4705d4d7b0903e0da935e8ee
Vetle Lode <vetlelode@gmail.com>
2017-04-06 13:38:22 +0200

    more stuff

--- bin/settings.ps1
+++ bin/settings.ps1
@@ -1,4 +1,5 @@
 adb devices
+adb shell settings put system setup_wizard_has_run 100
 adb shell settings put global install_non_market_apps 1
 Write-Output "Allowing Non Market Apps"
 adb shell settings put system screen_sleep 0
@@ -6,7 +7,10 @@ Write-Output "Disabling System Sleep"
 adb shell settings put global bluethooth_on 0
 Write-output "Disabling Bluethooth"
 adb shell settings put global wifi_on 1
-Write-Output "Disabling Sound Effects"
-adb shell settings put system sound_effects 0
-adb shell settings put global device_name PB-$lokasjon
-Write-Output "Setting device name to PB-$lokasjon"
+Write-Output "Disabling Sound effects"
+adb shell settings put system sound_effects_enabled 0
+Write-Output "Disabling haptic feedback"
+adb shell settings put system haptic_feedback_enabled 0
+Write-Output "Putting screen time out on max"
+adb shell settings put system screen_off_timeout 10000000000
+adb shell settings put global stay_on_while_plugged_in BATTERY_PLUGGED_AC
--- master_vetle.ps1
+++ master_vetle.ps1
@@ -6,6 +6,8 @@ Write-Output "Passord er: $wifipass"
 
 <# Settings #>
 powershell.exe .\bin\settings.ps1
+adb shell settings put global device_name PB-$lokasjon
+Write-Output "Setting device name to PB-$lokasjon"
 Write-output "Done with settings"
 <# Install apps #>
 powershell.exe .\bin\installer.ps1
